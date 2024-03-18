eda37a4d669ae8cb96b7905e52d76e5f0d24bbfa
Mateus <mateusff2323@gmail.com>
2024-03-17 21:00:58 -0300

    update

--- src/main/java/classes_nao_executaveis/Main.java
+++ src/main/java/classes_nao_executaveis/Main.java
@@ -8,34 +8,25 @@ public class Main {
 
     public static void main(String[] args) {
 
-//        // criando um objeto
-//        Cachorro cachorro1;
-//        cachorro1 = new Cachorro();
-//        // pode-se criar desta maneira tambem: Cachorro cachorro1 = new Cachorro();
-//
-//        // definindo os atributos do objeto
-//        cachorro1.nome = "Puppy";
-//        cachorro1.cor = "Marrom";
-//        cachorro1.altura = 25;
-//        cachorro1.tamanhoDoRabo = 5;
-//        cachorro1.peso = 5.7;
-//
-//        // System.out.println(cachorro1); // irá mostra o endereço do objeto na memória da JVM
-//
-//        cachorro1.latir();// imprimi AU AU
-//
-//        System.out.println(cachorro1.pegar());
-//
-//        // interagindo com o cachorro
-//        System.out.println("O cachorro está " + cachorro1.interagir("chute"));
+        // criando um objeto
 
-        Gato gato1 = new Gato();
-        gato1.nome = "Nani";
-        gato1.cor = "Branquinha";
-        gato1.idade = 7;
-        gato1.tamanho = 34.3;
+        Cachorro cachorro1 =
+                new Cachorro(); // padrão
 
-        System.out.println(gato1.pegarRato(45));
-        gato1.pularNoTelhado();
+        Cachorro cachorro2 =
+                new Cachorro("Puppy", "Marrom", 25, 5.7, 5);
+
+        Cachorro cachorro3 =
+                new Cachorro("Rex", "Marrom", 25, 5.7, 5);
+
+        System.out.println(cachorro1.getNumerosCachorros());
+
+        // pode ver que eles possuem endereço de memoria diferentes, mesmo que tenha os mesmos atributos:
+        System.out.println(cachorro2.toString());
+        System.out.println(cachorro3.toString());
+
+        System.out.println("-------------------");
+        System.out.println(cachorro2.toString());
+        System.out.println(cachorro3.toString());
     }
 }
